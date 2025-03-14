13a8b9c358d2afba5b367d99ef074b6007e1e052
Sudil Shrestha <sudilshr@gmail.com>
2025-03-14 14:33:23 +1100

    Update ESLint configuration to enable ES6 support and adjust parser options

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,33 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true // Add this line to enable ES6 support
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {  // Add this section
-        "ecmaVersion": 2018, // or 2020, 2021, etc.  Use the year corresponding to the ES version you want to support
-        "sourceType": "module" // Add this line to specify module type
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": "off",
-        
-    }
-};
+{
+    module.exports = {
+        "env": {
+            "browser": true,
+            "es6": true // Add this line to enable ES6 support
+        },
+        "extends": "eslint:recommended",
+        "parserOptions": {
+            "ecmaVersion": 8,
+            "sourceType": "module"
+          },
+        "rules": {
+            "indent": [
+                "error",
+                "tab"
+            ],
+            "linebreak-style": [
+                "error",
+                "unix"
+            ],
+            "quotes": [
+                "error",
+                "double"
+            ],
+            "semi": [
+                "error",
+                "always"
+            ],
+            "no-console": "off",
+
+        }
+    };
+}
